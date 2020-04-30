980aeb508a803ba4d07a06b66afac341965c8c65
Dror Tal <dror.tal@ls-techs.com>
2020-04-30 19:19:25 +0300

    Add filter and mark options

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
 const stands = 16;
-const values = [0, 18, 27, 35];
+const values = [9, 18, 27, 35];
+const mark = [0, 0.5, 1.0, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5];
 
 interface Iprops {}
 
@@ -13,6 +14,8 @@ interface Istate {
   link: string;
   stats: { name: string; level: string }[];
   name: string[];
+  chose: number;
+  filter: boolean;
 }
 
 class App extends Component<Iprops, Istate> {
@@ -22,20 +25,36 @@ class App extends Component<Iprops, Istate> {
       link: "",
       stats: [],
       name: [],
+      chose: 8,
+      filter: false,
     };
   }
 
-  readTeam() {
-    axios
-      .get("https://cors-anywhere.herokuapp.com/" + this.state.link)
+  async try() {
+    let i = 1;
+    while (i < 1945) {
+      try {
+        await this.readTeam(`https://www.total-football.org/team/${i}/squad/`);
+      } catch {
+        console.log(i);
+        break;
+      }
+      i += 1;
+    }
+  }
+
+  async readTeam(loopLink?: string) {
+    const str = loopLink ? loopLink : this.state.link;
+    await axios
+      .get("https://cors-anywhere.herokuapp.com/" + str)
       .then((res) => {
         const responseText = res.data;
         let counter = 0;
         let follower = 0;
         const find = "data-age";
         const findAfter = `name="`;
-        const temp = [];
-        const names: string[] = [];
+        const temp = this.state.stats;
+        const names: string[] = this.state.name;
         while (counter < responseText.length) {
           if (responseText[counter] === find[follower]) {
             follower++;
@@ -141,7 +160,7 @@ class App extends Component<Iprops, Istate> {
           }
         }
         counter += 2;
-        const temp = [];
+        const temp = this.state.stats;
         if (counter < responseText.length - 1) {
           let level: string = "";
           let name: string = "";
@@ -162,7 +181,9 @@ class App extends Component<Iprops, Istate> {
             counter++;
           }
         }
-        this.setState({ stats: temp, link: "", name: [name] });
+        const names = this.state.name;
+        names.push(name);
+        this.setState({ stats: temp, link: "", name: names });
       });
   }
 
@@ -183,6 +204,13 @@ class App extends Component<Iprops, Istate> {
         <Header className="my-header" size="huge">
           Total Reader
         </Header>
+        <Button
+          onClick={() => {
+            this.setState({ name: [], stats: [] });
+          }}
+        >
+          Delete All
+        </Button>
         <Form
           onSubmit={() => {
             let temp = this.state.link.length;
@@ -197,7 +225,10 @@ class App extends Component<Iprops, Istate> {
                 this.readPage();
               } else if (help[help.length - 2] === "squad") {
                 this.readTeam();
+              } else if (this.state.link === "All") {
+                this.try();
               }
+              console.log(help);
             }
           }}
         >
@@ -208,16 +239,38 @@ class App extends Component<Iprops, Istate> {
             <Input
               className="my-input"
               value={this.state.link}
-              placeholder="Link to the player..."
+              placeholder="Link to a player/team..."
               onChange={(e) => {
                 this.setState({ link: e.target.value });
               }}
             />
           </Form.Field>
         </Form>
+        <Button
+          onClick={() => {
+            this.setState({ filter: !this.state.filter });
+          }}
+        >
+          {this.state.filter ? "Filter" : "Mark"}
+        </Button>
+        {mark.map((value) => {
+          return (
+            <Button
+              color={this.state.chose === value ? "yellow" : "red"}
+              onClick={() => {
+                this.setState({ chose: value });
+              }}
+            >
+              {value}
+            </Button>
+          );
+        })}
         <div className="player-info">
           {this.state.name.map((name, index) => {
-            return (
+            return !this.state.filter ||
+              this.calculateStars(
+                Number(this.state.stats[index * (stands - 1)].level)
+              ) >= this.state.chose ? (
               <Card className="my-card">
                 <div>
                   <Header textAlign="center" className="info-name" size="small">
@@ -232,7 +285,13 @@ class App extends Component<Iprops, Istate> {
                     return (
                       <Header
                         key={position.name}
-                        className="info-header"
+                        className={
+                          !this.state.filter &&
+                          this.state.chose ===
+                            this.calculateStars(Number(position.level))
+                            ? "info-header-mark"
+                            : "info-header"
+                        }
                         size="small"
                       >
                         {position.name} : {position.level}, stars:{" "}
@@ -244,7 +303,7 @@ class App extends Component<Iprops, Istate> {
                   }
                 })}
               </Card>
-            );
+            ) : null;
           })}
         </div>
       </div>
